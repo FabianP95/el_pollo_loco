904f5ea8249c5e1215e35c9904b25b953a2b6943
Fabian Puchta <fabianpuchta@hotmail.de>
2026-02-17 20:31:27 +0100

    fix: added futher background img to expand level width - added style for h1

--- classes/levels/level1.js
+++ classes/levels/level1.js
@@ -30,6 +30,11 @@ function loadLevel() {
                                 new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 2),
                                 new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 2),
                                 new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 2),
+
+                                new BackgroundObject('img/5_background/layers/air.png', 720 * 3),
+                                new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 3),
+                                new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 3),
+                                new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 3),
                         ],
                         [new Coin(),
                         new Coin(),
